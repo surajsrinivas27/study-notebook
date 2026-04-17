1fa381358044d83b7a0990cda2b75a2d2afbc8ec
surajsrinivas27 <you@example.com>
2026-04-17 15:28:14 +0530

    added server.js

--- frontend/src/components/ChatWindow.jsx
+++ frontend/src/components/ChatWindow.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import TypingAnimation from './TypingAnimation'
 
 function ChatWindow({ messages }) {
@@ -52,5 +52,3 @@ function ChatWindow({ messages }) {
 }
 
 export default ChatWindow
-
-import { AnimatePresence } from 'framer-motion'
